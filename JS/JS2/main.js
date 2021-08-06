21ebe7a632da5b315a9eea1fe4c48ccf0fab7940
seungyoonLee <syoon624@naver.com>
2021-08-06 21:06:52 +0900

    JS npm parcel 환경 구축, test 용 export 함수

--- JS/JS2/main.js
+++ JS/JS2/main.js
@@ -1,1 +1,7 @@
 // type of - 자료형 출력
+import getType from './getType.js';
+
+console.log(getType(123)); // [object Number] -> Number
+console.log(getType(false)); // [object Boolean] -> Boolean
+console.log(getType({})); //Object
+console.log(getType([])); //Array
