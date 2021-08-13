101c04abbe1357251e87308ed8baa9a8f4e66fad
seungyoonLee <syoon624@naver.com>
2021-08-13 20:44:10 +0900

    scope변수와  import, 랜덤 함수

--- JS/JS2/getType.js
+++ JS/JS2/getType.js
@@ -1,3 +1,19 @@
 export default function getType(data) {
   return Object.prototype.toString.call(data).slice(8, -1);
 }
+
+export function random() {
+  //floor 정수 반환
+  return Math.floor(Math.random() * 10);
+}
+
+export const scope = () => {
+  if (true) {
+    var b = 321; // 함수 레벨 스코프 (잘 쓰지 않음.)
+    const a = 123; // let도 동일하다.(블록 레벨 스코프)
+    console.log('a 는 해당 블록 안에서만 동작합니다.');
+    console.log('a = ' + a);
+  }
+  console.log('b 는 함수 안에서만 동작합니다.');
+  console.log('b = ' + b);
+};
--- JS/JS2/main.js
+++ JS/JS2/main.js
@@ -1,7 +1,15 @@
 // type of - 자료형 출력
 import getType from './getType.js';
+import { random, scope } from './getType.js';
 
 console.log(getType(123)); // [object Number] -> Number
 console.log(getType(false)); // [object Boolean] -> Boolean
 console.log(getType({})); //Object
 console.log(getType([])); //Array
+
+// 산술 연산자(arithmetic operator)
+const a = random();
+
+console.log(a);
+
+console.log(scope());
