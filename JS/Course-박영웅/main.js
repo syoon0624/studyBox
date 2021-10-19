ba1ba1cc4d66bba72d4d67737edc97837ca2c394
seungyoonLee <syoon624@naver.com>
2021-10-19 16:32:03 +0900

    배열 많이쓰이는 메서드

--- JS/Course-박영웅/main.js
+++ JS/Course-박영웅/main.js
@@ -1,3 +1,8 @@
 import { arr } from './sub.js';
 
+// 1, 2, 3, 4, 19, 18, 61, 27;
+arr.sort((a, b) => {
+  return b - a;
+});
 console.log(arr);
+//1.filter 2.foreach 3.map 4.push 5.reduce 6.sum 7.splice
