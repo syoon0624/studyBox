69ed50d7843ac4d4cf7e596ecedb6a9fde2f9af7
seungyoonLee <syoon624@naver.com>
2021-10-19 13:40:12 +0900

    표기법

--- JS/Course-박영웅/main.js
+++ JS/Course-박영웅/main.js
@@ -1,18 +1,8 @@
-const user = {
-  name: 'HEROPY',
-  age: 12345,
-  email: 'thesecon@gmail.com',
-  getAge: () => {
-    console.log(this);
-  },
-};
+/*
+1. hello-world // dash-case, kebab-case
+2. hello_world // snake_case
 
-const userB = {
-  name: 'Neo',
-};
-
-console.log(user.getAge());
-console.log(user.getAge.call(userB));
-// this
-// 일반 함수에서는 호출 위치에서 정의됨
-// 화살표함수에서는 선언 위치에서 정의됨
+JS에서 주로 많이 씀
+3. helloWorldGoodLazy // camelCase
+4. HelloWorld // PascalCase
+ */
