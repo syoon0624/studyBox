5c4bfbc0751cfd15836dcd8c1911d5ed9f9c22f2
seungyoonLee <syoon624@naver.com>
2021-10-19 21:45:24 +0900

    데이터의 불변성과 가변성, 깊은 복사와 얕은 복사

--- JS/Course-박영웅/main.js
+++ JS/Course-박영웅/main.js
@@ -1,35 +1,23 @@
-//구조 분해 할당
-const arr1 = [1, 2, 3];
+//불변성
+//[1, 2, 3, 4, 5, 6] <- 메모리 주소
+//[ |  |  |  |  |  ] <- 데이터
+let a = 'xy';
+let b = a; //b도 1번 메모리 주소를 바라봄
+b; // 'xy'
+a; // 'xy'
+b = 123; // 원시형 데이터는 새로운 메모리주소에 할당됨(불변성)
 
-//[, y, z]도 가능
-const [x, y, z] = arr1;
-console.log(x, y, z);
+//객체의 경우
+let a = { x: 1 };
+let b = a;
+b.x = 7; // 참조형 데이터는 같은 메모리주소를 바라보는 데이터 값을 변경(가변성)
 
-//객체 데이터 내부의 순서는 중요하지도 않고, 보장되지도 않는다. -> Key값이란 것이 있기 때문에
-const objA = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 6,
-  e: 9,
-};
-//key값을 그대로 써야 함. 이미 있는 value값은 재할당 x
-const { a: heropy, c, d = 4, ...rest } = objA;
-// const { c, a } = objA 도 가능
-console.log(heropy, c, d, rest);
-
-// 매개변수에 기본값 사용(재할당 x)
-function xyz(a = 1) {
-  return a * 2;
-}
-console.log(xyz(3));
-console.log(xyz());
-
-//기본값 사용2
-function abc(o = { name: 'hello' }) {
-  //optional chaining: 조회가 불가한 항목을 undefined처리
-  return o?.name?.toUpperCase();
-}
-
-console.log(abc({ name: 'HEROPY' }));
-console.log(abc());
+let a = { x: 1, y: 2 };
+let b = { y: 3, z: 4 };
+//전개 연산자의 경우, 제일 바깥의 데이터만 깊은 복사를 실행하게 되어, 더 깊은 참조의 경우, 얕은 복사를 하게 된다.(같은 메모리주소를 바라봄.)
+let c = { ...a };
+let d = a;
+c.x = 4;
+d.x = 2;
+console.log(c.x);
+console.log(a.x);
