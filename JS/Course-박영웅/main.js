e427a2ebd102e142463578135be7ced7c3ab25f9
seungyoonLee <syoon624@naver.com>
2021-10-19 19:18:48 +0900

    prototype 응용

--- JS/Course-박영웅/main.js
+++ JS/Course-박영웅/main.js
@@ -1,48 +1,23 @@
-//this
-const timer = {
-  timerName: 'HEROPY',
-  timeout: function () {
-    //이 함수는 호출된  timer라는 대상을 this로 지정
-    console.log(this.timerName);
-    //이 화살표 함수는 호출된 setTimeout()이라는 함수 상관 없이, timer라는 선언된 곳을 this로 지정
-    setTimeout(() => {
-      console.log(this.timerName);
-    }, 1000);
-  },
+Array.prototype.double = function () {
+  return this.map(function (item) {
+    return item * 2;
+  });
 };
-const timerB = {
-  timerName: 'Leon',
+Object.prototype.heropy = function () {
+  Object.keys(this).forEach(key => {
+    this[key] *= 2;
+  });
 };
 
-timer.timeout();
-timer.timeout.call(timerB);
+const arr = [3, 4, 5, 6];
+const doubledArr = arr.double();
+console.log(doubledArr);
 
-//prototype
-function User(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-}
-User.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
+const obj = {
+  a: 3,
+  b: 4,
+  c: 5,
 };
 
-const amy = new User('Amy', 'Jung');
-console.log(amy.getFullName());
-
-//객체 리터럴
-const user = {
-  firstName: 'HEROPY',
-  lastName: 'Park',
-  getFullName: function () {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
-
-console.log(user.getFullName());
-
-const leon = {
-  firstName: 'Leon',
-  lastName: 'Miller',
-};
-
-console.log(user.getFullName.call(leon));
+obj.heropy();
+console.log(obj);
