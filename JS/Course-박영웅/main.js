69ff805410b9a461728755ad01c87d713f3803b5
seungyoonLee <syoon624@naver.com>
2021-10-20 21:21:33 +0900

    비동기 패턴 : promise, async

--- JS/Course-박영웅/main.js
+++ JS/Course-박영웅/main.js
@@ -1,21 +1,56 @@
-//비동기 처리 예 1. 콜백 함수
-function a(callback) {
-  setTimeout(() => {
-    console.log('a');
-    callback();
+//비동기 처리 예 2. promise
+function a() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('a');
+      resolve();
+    });
   });
 }
-function b(callback) {
-  setTimeout(() => {
-    console.log('b');
-    callback();
+function b() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('b');
+      resolve();
+    });
   });
 }
 function c() {
-  console.log('c');
-}
-a(() => {
-  b(() => {
-    c();
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('c');
+      resolve();
+    });
   });
-});
+}
+function d() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('d');
+      resolve();
+    });
+  });
+}
+function e() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('e');
+      resolve();
+    });
+  });
+}
+
+a()
+  .then(() => b())
+  .then(() => c())
+  .then(() => d())
+  .then(() => e());
+
+async function x() {
+  await a();
+  await b();
+  await c();
+  await d();
+  await e();
+}
+x();
