73179f619afb14eea03cedf69386dc916b0b4553
seungyoonLee <syoon624@naver.com>
2021-10-21 14:03:49 +0900

    비동기: try & catch

--- JS/Course-박영웅/main.js
+++ JS/Course-박영웅/main.js
@@ -1,15 +1,57 @@
-import { loadImage } from './utils/index.js';
-const url =
-  'https://m.media-amazon.com/images/M/MV5BMjA0YjYyZGMtN2U0Ni00YmY4LWJkZTItYTMyMjY3NGYyMTJkXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_SX1300.jpg';
+const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=7035c60c';
 
-const loadingEl = document.querySelector('.loading');
+// async function fetchMovie() {
+//   try {
+//     const res = await fetch(url);
+//     return await res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-loadImage(url).then(() => {
-  loadingEl.remove();
-});
+// function fetchMovie() {
+//   return new Promise(resolve => {
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(res => resolve(res));
+//   });
+// }
 
-//또는
+function fetchMovie() {
+  return new Promise(async (resolve, reject) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (data.Error) {
+      reject(data.Error);
+    }
+    resolve(data);
+  });
+}
+
+fetchMovie()
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+    const errEl = document.createElement('div');
+    errEl.textContent = err;
+    document.body.append(errEl);
+  })
+  .finally(() => {
+    console.log('Done!');
+  });
+
 (async function () {
-  await loadImage(url);
-  loadingEl.remove();
+  try {
+    const movie = await fetchMovie();
+    console.log(movie);
+  } catch (errorMsg) {
+    console.log(errorMsg);
+    const errorEl = document.createElement('div');
+    errorEl.textContent = errorMsg;
+    document.body.append(errorEl);
+  } finally {
+    console.log('Done!');
+  }
 })();
