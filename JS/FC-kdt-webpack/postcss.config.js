e8bc98fb9e59bd2947d259437251d36b76741a53
seungyoonLee <syoon624@naver.com>
2021-10-22 16:16:00 +0900

    scss/favicon 세팅

---
+++ JS/FC-kdt-webpack/postcss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins: [require('autoprefixer')],
+};
+// browserslist 등록해야함.(필 수 !)
--- JS/FC-kdt-webpack/webpack.config.js
+++ JS/FC-kdt-webpack/webpack.config.js
@@ -1,7 +1,11 @@
 const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
   return {
+    resolve: {
+      extensions: ['.js'],
+    },
     entry: './src/main.js',
     output: {
       //path: '',
@@ -9,10 +13,31 @@ module.exports = (env, options) => {
       publicPath: '/',
       clean: true,
     },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+      ],
+    },
     plugins: [
       new HtmlPlugin({
         template: './src/index.html',
       }),
+      new CopyPlugin({
+        patterns: [{ from: 'static', to: '' }],
+      }),
     ],
+    devServer: {
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
   };
 };
