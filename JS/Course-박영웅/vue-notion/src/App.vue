87b3003653ede734ef449fa8a0bd000beb822271
seungyoonLee <syoon624@naver.com>
2021-11-12 19:44:01 +0900

    dayjs

--- JS/Course-박영웅/vue-notion/src/App.vue
+++ JS/Course-박영웅/vue-notion/src/App.vue
@@ -4,11 +4,23 @@
 </template>
 
 <script>
+import dayjs from 'dayjs'
 import TheHeader from '~/components/TheHeader'
 
 export default {
   components: {
     TheHeader
+  },
+  created() {
+    const iso = dayjs(dayjs('2021-11-12 20:00:00').format('YYYY-MM-DD HH:mm:ss')).toISOString()
+    console.log(iso)
+
+    const formatted = dayjs('2021-11-12T11:00:00.000Z').format('YYYY년 MM월 DD일 HH시 mm분 ss초')
+    console.log(formatted)
+
+    console.log(dayjs()) // 현재 시간 dayjs객체
+    console.log(dayjs().isBefore('2021-11-12T11:00:00.000Z')) //시간 전후시간대에 따라 Boolean 출력
+    
   }
 }
 </script>
