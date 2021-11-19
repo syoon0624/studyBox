25de116fa82a4e077df913d31a4a42f4f24b02ba
seungyoonLee <syoon624@naver.com>
2021-11-19 20:56:59 +0900

    life cycle: state

--- JS/Course-react/my-app/src/App.js
+++ JS/Course-react/my-app/src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import TodayPlan from './component/TodayPlan';
 import MyComponent from './component/MyComponent';
 import C11_StateExample from './component/StateExample';
 import LifeCycle from './component/LifeCycle';
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [destroy, setDestroy] = useState(false);
+
+  //ComponentDidMount 호출
+  useEffect(() => {
+    setDestroy(!destroy);
+  },[])
+
   return (
     <div className="App">
-      <TodayPlan />
+      {/* <TodayPlan />
       <MyComponent name="패스트 캠퍼스" />
-      <C11_StateExample />
-      <LifeCycle />
+      <C11_StateExample /> */}
+      { destroy ? null : <LifeCycle /> }
     </div>
   );
 }
