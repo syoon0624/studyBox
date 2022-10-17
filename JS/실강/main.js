0a76051dac3928fda90af93d5c7858284c2c64ca
seungyoonLee <syoon624@naver.com>
2022-10-17 19:25:18 +0900

    this: 개요

--- JS/실강/main.js
+++ JS/실강/main.js
@@ -1,25 +1,29 @@
-// 화살표 함수: 선언으로 만들 수 x, 표현!!
+// this
 
-const sum1 = function(a,b) {
-    return a + b
+function user() {
+  this.firstName = 'Neo'
+  this.lastName = 'Anderson'
+  
+  return {
+    firstName: 'Heropy',
+    lastName: 'Park',
+    age: 85,
+    getFullName: () => {
+      return `${this.firstName} ${this.lastName}`
+    },
+    getFullName2: function() {
+      return `${this.firstName} ${this.lastName}`
+    } 
+  }
 }
 
-//화살표 함수(중괄호가 없다면 바로 return)
-const sum2 = (a,b) => a + b
-//소괄호 생략
-const log = c => console.log(c)
+const u = user()
+console.log(u.getFullName())
+console.log(u.getFullName2())
 
-//함수 표들
-const a = () => {} // 매개변수가 없을 경우
-const b = x => {} // 매개변수 1개는 소괄호 생략 가능
-const c = (x, y) => {} //매개변수가 없거나 2개 이상은 소괄호를 생략 불가
-const d = x => { return x * x }
-const e = x => x * x
-const f = x => {
-    console.log(x * x)
-    return x * x
+const amy = {
+  firstName: 'Amy',
+  lastName: 'Clarke'
 }
-const g = () => {return {a : 1}}
-const h = () => ({a: 1}) //객체 데이터 활용
-const i = () => {return [1, 2, 3]}
-const j = () => [1, 2, 3]
+
+console.log(u.getFullName2.call(amy)) // 'Amy Clarke'
