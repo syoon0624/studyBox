799a818efe8aa5866a3c2c15990791df34cf9f64
seungyoonLee <syoon624@naver.com>
2022-10-17 19:46:30 +0900

    this: 예제

--- JS/실강/main.js
+++ JS/실강/main.js
@@ -1,29 +1,45 @@
-// this
-
-function user() {
-  this.firstName = 'Neo'
-  this.lastName = 'Anderson'
-  
-  return {
-    firstName: 'Heropy',
-    lastName: 'Park',
-    age: 85,
-    getFullName: () => {
-      return `${this.firstName} ${this.lastName}`
-    },
-    getFullName2: function() {
-      return `${this.firstName} ${this.lastName}`
-    } 
+let newC
+let newD
+// 객체 리터럴 선언!
+const literal = {
+  a: 1,
+  b: 2,
+  // 일반 함수는 호출 위치에서 따라 this 정의!
+  c: function () {
+    console.log(this.a)
+  },
+  // 화살표 함수는 this가 자신이 선언된 렉시컬(함수) 범위에서 this 정의!
+  d: () => {
+    console.log(this.a)
   }
 }
+literal.c() // 1
+literal.d() // undefined
+newC = literal.c // getter: 값을 호출해주는 선언
+newD = literal.d
+newC() // undefined
+newD() // undefined
+newC.call(literal) // 1
+newD.call(literal) // undefined
 
-const u = user()
-console.log(u.getFullName())
-console.log(u.getFullName2())
-
-const amy = {
-  firstName: 'Amy',
-  lastName: 'Clarke'
+// 인스턴스 생성!
+const instance = new function () {
+  this.a = 3
+  this.b = 4
+  // 일반 함수는 호출 위치에서 따라 this 정의!
+  this.c = function () {
+    console.log(this.a)
+  }
+  // 화살표 함수는 this가 자신이 선언된 렉시컬(함수) 범위에서 this 정의!
+  this.d = () => {
+    console.log(this.a)
+  }
 }
-
-console.log(u.getFullName2.call(amy)) // 'Amy Clarke'
+instance.c() // 3
+instance.d() // 3
+newC = instance.c
+newD = instance.d
+newC() // undefined
+newD() // 3
+newC.call(instance) // 3
+newD.call(instance) // 3
