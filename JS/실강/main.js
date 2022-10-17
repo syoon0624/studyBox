56ec96ff9dd55c3e22807f685063318484f56341
seungyoonLee <syoon624@naver.com>
2022-10-17 20:40:39 +0900

    call / apply / bind

--- JS/실강/main.js
+++ JS/실강/main.js
@@ -1,45 +1,21 @@
-let newC
-let newD
-// 객체 리터럴 선언!
-const literal = {
-  a: 1,
-  b: 2,
-  // 일반 함수는 호출 위치에서 따라 this 정의!
-  c: function () {
-    console.log(this.a)
-  },
-  // 화살표 함수는 this가 자신이 선언된 렉시컬(함수) 범위에서 this 정의!
-  d: () => {
-    console.log(this.a)
+const heropy = {
+  name: 'Heropy'
+}
+const amy = {
+  name: 'Amy',
+  getName(age) {
+    return `${this.name} is ${age}`
   }
 }
-literal.c() // 1
-literal.d() // undefined
-newC = literal.c // getter: 값을 호출해주는 선언
-newD = literal.d
-newC() // undefined
-newD() // undefined
-newC.call(literal) // 1
-newD.call(literal) // undefined
 
-// 인스턴스 생성!
-const instance = new function () {
-  this.a = 3
-  this.b = 4
-  // 일반 함수는 호출 위치에서 따라 this 정의!
-  this.c = function () {
-    console.log(this.a)
-  }
-  // 화살표 함수는 this가 자신이 선언된 렉시컬(함수) 범위에서 this 정의!
-  this.d = () => {
-    console.log(this.a)
-  }
-}
-instance.c() // 3
-instance.d() // 3
-newC = instance.c
-newD = instance.d
-newC() // undefined
-newD() // 3
-newC.call(instance) // 3
-newD.call(instance) // 3
+// call
+console.log(
+  amy.getName.call(heropy, 85)
+)
+// apply
+console.log(
+  amy.getName.apply(heropy, [85])
+)
+// bind
+const heropysGetName = amy.getName.bind(heropy)
+console.log(heropyGetName(85))
