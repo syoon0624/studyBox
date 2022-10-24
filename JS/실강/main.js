760fe6b7103cc4305fbfd3a06ac426f39cce4ad7
seungyoonLee <syoon624@naver.com>
2022-10-24 21:42:14 +0900

    영화 API 예제

--- JS/실강/main.js
+++ JS/실강/main.js
@@ -1,21 +1,44 @@
-const heropy = {
-  name: 'Heropy'
+;(async () => {
+  // 초기화
+  const moreBtnEl = document.querySelector('.btn')
+  let page = 1
+
+  // 최초 호출
+  const movies = await getMovies()
+  page += 1
+  renderMovies(movies);
+
+  moreBtnEl.addEventListener('click', async () => {
+    const movies = await getMovies(page)
+    page += 1  
+    renderMovies(movies);
+  })
+
+})()
+
+async function getMovies(page = 1) {
+  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=frozen&page=${page}`)
+  const { Search: movies } = await res.json()
+  return movies
 }
-const amy = {
-  name: 'Amy',
-  getName(age) {
-    return `${this.name} is ${age}`
+
+function renderMovies(movies) {
+  const moviesEl = document.querySelector('.movies')
+  
+  for (const movie of movies) {
+    const el = document.createElement('div')
+    el.classList.add('movie')
+
+    el.innerHTML = /* html */ `
+      <h1>${movie.Title}</h1>
+      <img src="${movie.Poster}" />
+    `
+
+    const h1El = el.querySelector('img')
+    h1El.addEventListener('click', () => {
+      console.log(movie.Title)
+    })
+    moviesEl.append(el)
   }
 }
 
-// call
-console.log(
-  amy.getName.call(heropy, 85)
-)
-// apply
-console.log(
-  amy.getName.apply(heropy, [85])
-)
-// bind
-const heropysGetName = amy.getName.bind(heropy)
-console.log(heropyGetName(85))
