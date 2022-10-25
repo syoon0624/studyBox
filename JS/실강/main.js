1820d1ef20549dde742449c5c8ca2ef84d1cb69e
seungyoonLee <syoon624@naver.com>
2022-10-25 19:02:24 +0900

    영화검색 사이트: 더보기 중지 기능

--- JS/실강/main.js
+++ JS/실강/main.js
@@ -2,6 +2,7 @@
   // 초기화
   const moreBtnEl = document.querySelector('.btn')
   let page = 1
+  let maxPage = -1
 
   // 최초 호출
   const movies = await getMovies()
@@ -9,36 +10,45 @@
   renderMovies(movies);
 
   moreBtnEl.addEventListener('click', async () => {
-    const movies = await getMovies(page)
-    page += 1  
-    renderMovies(movies);
+    console.log(2)
+    if(page >= maxPage) {
+      alert('마지막 페이지입니다.')
+    } else {
+      const movies = await getMovies(page)
+      page += 1  
+      renderMovies(movies);
+    }
   })
 
+  
+  async function getMovies(page = 1) {
+    const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=avengers&page=${page}`)
+    const { Search: movies, totalResults } = await res.json()
+    maxPage = Math.ceil(Number(totalResults))
+    console.log(1)
+    return movies
+  }
+
+  function renderMovies(movies) {
+    const moviesEl = document.querySelector('.movies')
+  
+    for (const movie of movies) {
+      const el = document.createElement('div')
+      el.classList.add('movie')
+      el.innerHTML = /* html */ `
+        <h1>${movie.Title}</h1>
+        <img src="${movie.Poster}" />
+      `
+  
+      const h1El = el.querySelector('img')
+      h1El.addEventListener('click', () => {
+        console.log(movie.Title)
+      })
+      moviesEl.append(el)
+    }
+  }  
 })()
 
-async function getMovies(page = 1) {
-  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=frozen&page=${page}`)
-  const { Search: movies } = await res.json()
-  return movies
-}
 
-function renderMovies(movies) {
-  const moviesEl = document.querySelector('.movies')
-  
-  for (const movie of movies) {
-    const el = document.createElement('div')
-    el.classList.add('movie')
 
-    el.innerHTML = /* html */ `
-      <h1>${movie.Title}</h1>
-      <img src="${movie.Poster}" />
-    `
-
-    const h1El = el.querySelector('img')
-    h1El.addEventListener('click', () => {
-      console.log(movie.Title)
-    })
-    moviesEl.append(el)
-  }
-}
 
