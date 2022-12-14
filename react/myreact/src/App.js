be7a80e2721e605083b86a9ade0ecf47136b6180
seungyoonLee <syoon624@naver.com>
2022-12-14 20:37:57 +0900

    TicTacToc 앱 완성

--- react/myreact/src/App.js
+++ react/myreact/src/App.js
@@ -5,10 +5,26 @@ import Board from './components/Board';
 const App = () => {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
-  const current = history[history.length - 1];
+  const [stepNumber, setStepNumber] = useState(0);
+  const current = history[stepNumber];
 
   let status = `Next player: ${xIsNext ? 'X' : 'O'}`;
   const winner = calculateWinner(current.squares);
+
+  const jumpTo = (move) => {
+    setStepNumber(move);
+    setXIsNext(move % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move : 'go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   if (winner) {
     status = 'Winner: ' + winner;
   } else {
@@ -16,14 +32,18 @@ const App = () => {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
 
     if (calculateWinner(newSquares) || newSquares[i]) {
       return;
     }
     newSquares[i] = xIsNext ? 'X' : 'O';
-    setHistory([...history, { squares: newSquares }]);
+    setHistory([...newHistory, { squares: newSquares }]);
     setXIsNext((current) => !current);
+
+    setStepNumber(newHistory.length);
   };
 
   function calculateWinner(squares) {
@@ -63,7 +83,7 @@ const App = () => {
         <div className="status">{status}</div>
         <div></div>
         {/* todo */}
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
