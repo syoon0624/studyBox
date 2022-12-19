324844da1b4b34cb5c0ba8fcd4e4af10b191d0e1
seungyoonLee <syoon624@naver.com>
2022-12-19 14:36:19 +0900

    타입 단언

--- TS/src/main.ts
+++ TS/src/main.ts
@@ -1,58 +1,11 @@
-const a: string = 'Hello world!';
-const b: number = 123;
-const c: boolean = true;
-const d: number[] = [1, 2, 3];
-const e: Obj = {
-  name: 'Lee',
-  age: 85,
-  isValid: true,
-};
+// 타입 단언(Assertioins)
 
-interface Obj {
-  name: string;
-  age: number;
-  isValid: boolean;
+function getValue(x: string | number, isNumber: boolean) {
+  if (isNumber) {
+    return Number((x as number).toFixed(2));
+  }
+  return (x as string).toUpperCase();
 }
 
-// 튜플 지정
-const g: [number, number][] = [
-  [1, 2],
-  [3, 4],
-  [5, 6],
-];
-const h: unknown = 123;
-
-interface Objec {
-  [key: string]: unknown;
-}
-
-const Lee: Objec = {
-  a: 1,
-  b: 'Helowe',
-  c: true,
-};
-
-function log(a: number, b: number): number {
-  return a + b;
-}
-
-log(1, 2);
-
-// 유니온 타입
-let name: number | string = 1;
-name = 'hello';
-
-interface A {
-  a: string;
-}
-interface B {
-  b: string;
-}
-
-// 인터섹션 타입
-const Lees: A & B = {
-  a: 'a',
-  b: 'b',
-};
-
-export {};
+getValue('hello world', false); // 'hello world'
+getValue(3.141592, true); // 3.14
