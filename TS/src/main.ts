a5177758902f32c9eb7d79c913dc980f8e22e9b7
seungyoonLee <syoon624@naver.com>
2022-12-19 16:21:07 +0900

    TS: 타입 별칭

--- TS/src/main.ts
+++ TS/src/main.ts
@@ -1,48 +1,10 @@
-// 인터페이스(Inerface)
-
-interface User {
-  readonly name: string;
-  age: number;
-  // 선택적으로 있어도 되고, 없어도 되는 프로퍼티
-  isValid?: boolean;
-}
-
-const heropy: User = {
-  name: 'Hercules',
-  age: 30,
-  isValid: true,
-};
-
-const neo: User = {
-  name: 'Neo',
-  age: 102,
-};
-
-// 인덱스 시그니처
-interface Fruits {
-  [key: number]: string;
-}
-
-const fruits: Fruits = ['Apple', 'Orange', 'Banana'];
-console.log(fruits);
-
-// 인터페이스 확장 (Extends)
-interface UserA {
+// 타입 별칭(Type Alias)
+interface A {
   name: string;
   age: number;
 }
-interface UserB extends UserA {
-  isValid: boolean;
-}
 
-const heropys: UserA = {
-  name: 'Hercules',
-  age: 30,
-  isValid: true, // Error!
-};
+const a: Hello = 1;
+const b: Hello = 'Hello world';
 
-const neos: UserB = {
-  name: 'Neo',
-  age: 102,
-  isValid: true,
-};
+type Hello = string | number | A;
