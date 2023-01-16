5cc9889b7acadeb83a5edcb98f87e02da70e1a26
seungyoonLee <syoon624@naver.com>
2023-01-16 20:35:10 +0900

    mobx - todo app

--- react/redux-mobx/src/index.tsx
+++ react/redux-mobx/src/index.tsx
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CounterStore from './CounterStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const store = new CounterStore();
-
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <App myCounter={store} />
+    <App />
   </React.StrictMode>
 );
 
